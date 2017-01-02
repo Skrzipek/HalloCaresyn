85fe566805ca27d7431d349f58ba5dc60eb8cf35
oskrzipek <oliver.skrzipek@caresyn.com>
2017-01-02 17:38:48 +0100

    hellocaresyn

--- src/service/CaresynRouteServiceProvider.php
+++ src/service/CaresynRouteServiceProvider.php
@@ -10,10 +10,10 @@ class CaresynRouteServiceProvider extends RouteServiceProvider
 
   /**
    * Explanation: We use the get method to pass two parameters. The first parameter 'hello' defines the route.
-   * The second parameter consists of the Fully-Qualified Class Name and the @ controller method that is called when the route is called. 
+   * The second parameter consists of the Fully-Qualified Class Name and the @ controller method that is called when the route is called.
    */
   public function map( Router $router ) {
-    $router->get('hello', 'HelloCaresyn\content\CaresynContentController@sayCaresyn');
+    $router->get('hellocaresyn', 'HelloCaresyn\content\CaresynContentController@sayCaresyn');
   }
 
 }
