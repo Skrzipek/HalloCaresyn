83645dd74be0a25e3b54003c1815d7adee986f93
oskrzipek <oliver.skrzipek@caresyn.com>
2017-01-02 17:16:17 +0100

    testing 2

--- src/controller/CaresynContentController.php
+++ src/controller/CaresynContentController.php
@@ -10,7 +10,7 @@ class CaresynContentController extends Controller
 
   public function sayCaresyn( Twig $twig )
   {
-    return $twig->render('HelloCaresyn::hello.php');
+    return $twig->render('HelloCaresyn::hello.twig');
   }
 
 }
